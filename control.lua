c8419ba83ff87face2ef2afe184b2878cb112dba
Noah Morton <noahmorton98@gmail.com>
2017-07-07 17:02:50 -0500

    Added safeguard to debug in case I forget to comment it out again, updated info.json

--- control.lua
+++ control.lua
@@ -1,10 +1,15 @@
 require("bobsLogistics")
 
 function enableTesting(event)
-	game.players[event.player_index].cheat_mode = true
-	game.players[event.player_index].force.research_all_technologies()
+	local player = game.players[event.player_index]
+	if player.name ~= "Gangsir" then --safeguard, just in case I forget to comment this out
+		player.print("You aren't the dev. He must've forgot to comment this out. Sorry for polluting your commands with debug.")
+	else
+		player.cheat_mode = true
+		player.force.research_all_technologies()
+	end
 end
-commands.add_command("testing", "Run to enable testing without disabling achievements", enableTesting)
+commands.add_command("achieverTesting", "Run to enable testing of achiever without disabling achievements.", enableTesting)
 
 script.on_init(function(e)
 	global.trees_destroyed = 0
