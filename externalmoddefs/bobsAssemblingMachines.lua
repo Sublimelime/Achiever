c7191d6c24dc4c77cef6f7a748dfdc309c347568
Noah Morton <noahmorton98@gmail.com>
2017-07-26 10:55:58 -0500

    Added 4 more achievements for bob's assembling machines

---
+++ externalmoddefs/bobsAssemblingMachines.lua
@@ -0,0 +1,31 @@
+data:extend{
+	{
+		type = "build-entity-achievement",
+		name = "flash-crafter",
+		order = "a",
+		to_build = "assembling-machine-6",
+		icon = "__Achiever__/graphics/placeholder.png",
+	},
+	{
+		type = "build-entity-achievement",
+		name = "a-refined-taste",
+		order = "b",
+		to_build = "oil-refinery-4",
+		icon = "__Achiever__/graphics/placeholder.png",
+	},
+	{
+		type = "build-entity-achievement",
+		name = "we-have-chemistry",
+		order = "c",
+		to_build = "chemical-plant-4",
+		icon = "__Achiever__/graphics/placeholder.png",
+	},
+	{
+		type = "research-achievement",
+		name = "what-if-we-could-craft-even-faster",
+		order = "d",
+		technology = "automation-4",
+		icon = "__Achiever__/graphics/placeholder.png",
+	},
+
+}
