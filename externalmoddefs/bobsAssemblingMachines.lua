94dd313b57f3b680dea1f1240f31035a73121fdb
Noah Morton <noahmorton98@gmail.com>
2017-07-27 15:37:26 -0500

    Added icons for new achievements

--- externalmoddefs/bobsAssemblingMachines.lua
+++ externalmoddefs/bobsAssemblingMachines.lua
@@ -4,28 +4,28 @@ data:extend{
 		name = "flash-crafter",
 		order = "a",
 		to_build = "assembling-machine-6",
-		icon = "__Achiever__/graphics/placeholder.png",
+		icon = "__Achiever__/graphics/bobs/flash-crafter.png",
 	},
 	{
 		type = "build-entity-achievement",
 		name = "a-refined-taste",
 		order = "b",
 		to_build = "oil-refinery-4",
-		icon = "__Achiever__/graphics/placeholder.png",
+		icon = "__Achiever__/graphics/bobs/a-refined-taste.png",
 	},
 	{
 		type = "build-entity-achievement",
 		name = "we-have-chemistry",
 		order = "c",
 		to_build = "chemical-plant-4",
-		icon = "__Achiever__/graphics/placeholder.png",
+		icon = "__Achiever__/graphics/bobs/we-have-chemistry.png",
 	},
 	{
 		type = "research-achievement",
 		name = "what-if-we-could-craft-even-faster",
 		order = "d",
 		technology = "automation-4",
-		icon = "__Achiever__/graphics/placeholder.png",
+		icon = "__Achiever__/graphics/bobs/what-if-we-could-craft-even-faster.png",
 	},
 
 }
--- externalmoddefs/bobsMining.lua
+++ externalmoddefs/bobsMining.lua
@@ -4,14 +4,14 @@ data:extend{
 		name = "mining-the-area",
 		order = "a",
 		to_build = "bob-area-mining-drill-1",
-		icon = "__Achiever__/graphics/placeholder.png",
+		icon = "__Achiever__/graphics/bobs/mining-the-area.png",
 	},
 	{
 		type = "build-entity-achievement",
 		name = "no-ores-land",
 		order = "b",
 		to_build = "bob-area-mining-drill-4",
-		icon = "__Achiever__/graphics/placeholder.png",
+		icon = "__Achiever__/graphics/bobs/no-ores-land.png",
 	},
 	{
 		type = "produce-achievement",
@@ -19,7 +19,7 @@ data:extend{
 		order = "c",
 		item_product = "diamond-axe",
 		amount = 1,
-		icon = "__Achiever__/graphics/placeholder.png",
+		icon = "__Achiever__/graphics/bobs/can-i-axe-you-a-question.png",
 		limited_to_one_game = true,
 	},
 	{
@@ -27,13 +27,13 @@ data:extend{
 		name = "ultiminer",
 		order = "d",
 		to_build = "bob-mining-drill-4",
-		icon = "__Achiever__/graphics/placeholder.png",
+		icon = "__Achiever__/graphics/bobs/ultiminer.png",
 	},
 	{
 		type = "build-entity-achievement",
 		name = "pumped-up",
 		order = "d",
 		to_build = "bob-pumpjack-4",
-		icon = "__Achiever__/graphics/placeholder.png",
+		icon = "__Achiever__/graphics/bobs/pumped-up.png",
 	},
 }
