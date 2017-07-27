090fd6bf47e2e8d6bec1b211da689d252e20afb1
Noah Morton <noahmorton98@gmail.com>
2017-07-27 12:17:02 -0500

    Added settings to disable achievements by mod.

--- data.lua
+++ data.lua
@@ -1,15 +1,17 @@
-require("internal")
-require("scripted")
+if settings.startup["achiever-vanilla-achievements-support"].value == true then
+	require("internal")
+	require("scripted")
+end
 
 --Check if bob's mods are installed
-if data.raw["storage-tank"]["bob-valve"] ~= nil then --bob's logistics installed
+if settings.startup["achiever-bob-logistics-support"].value == true and data.raw["storage-tank"]["bob-valve"] ~= nil then --bob's logistics installed
 	require("externalmoddefs.bobsLogistics")
 end
 
-if data.raw["item-subgroup"]["bob-assembly-machine"] ~= nil then -- Bob's assembling machines installed
+if settings.startup["achiever-bob-assembly-support"].value == true and data.raw["item-subgroup"]["bob-assembly-machine"] ~= nil then -- Bob's assembling machines installed
 	require("externalmoddefs.bobsAssemblingMachines")
 end
 
-if data.raw.item["bob-area-mining-drill-1"] ~= nil then -- Bob's mining installed
+if settings.startup["achiever-bob-mining-support"].value == true and data.raw.item["bob-area-mining-drill-1"] ~= nil then -- Bob's mining installed
 	require("externalmoddefs.bobsMining")
 end
---
+++ settings.lua
@@ -0,0 +1,30 @@
+data:extend{
+  {
+	type = "bool-setting",
+	name = "achiever-bob-logistics-support",
+	setting_type = "startup",
+	default_value = true,
+	per_user = false,
+  },
+  {
+	type = "bool-setting",
+	name = "achiever-bob-assembly-support",
+	setting_type = "startup",
+	default_value = true,
+	per_user = false,
+  },
+  {
+	type = "bool-setting",
+	name = "achiever-bob-mining-support",
+	setting_type = "startup",
+	default_value = true,
+	per_user = false,
+  },
+  {
+	type = "bool-setting",
+	name = "achiever-vanilla-achievements-support",
+	setting_type = "startup",
+	default_value = true,
+	per_user = false,
+  },
+}
